5c43046796939ca6a9d6b594746d44c89e3dfee6
YskaArt <franciscomorello99@gmail.com>
2025-05-11 21:46:36 -0300

    Parte 2
    Se añade la funcion de poder atacar a los enemigos y que estos puedan recibir daño

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -1,17 +1,37 @@
 using UnityEngine;
 
-public class Enemigo : MonoBehaviour, IPresentacion
+public class Enemigo : MonoBehaviour, IPresentacion, ITakeDamage
 {
     [SerializeField] protected ScriptableEnemy datos;
 
-    public virtual void Accion()
-    {
-        
-    }
+    private int vidaActual;
 
     private void Start()
     {
-        Debug.Log($"Apareció un {datos.Nombre} con {datos.Vida} de vida.");
-        
+        vidaActual = datos.Vida;
+        Debug.Log($"Apareció un {datos.Nombre} con {vidaActual} de vida.");
+       
+    }
+
+    public virtual void Accion()
+    {     
+        //Esto se reescribe dentro de cada Enemigo
+    }
+
+    public void TomarDaño(int cantidad)
+    {
+        vidaActual -= cantidad;
+        Debug.Log($"{datos.Nombre} recibió {cantidad} de daño. Vida restante: {vidaActual}");
+
+        if (vidaActual <= 0)
+        {
+            Morir();
+        }
+    }
+
+    protected virtual void Morir()
+    {
+        Debug.Log($"{datos.Nombre} ha muerto.");
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -1,26 +1,58 @@
-
 using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
     public float velocidad = 5f;
     public float rangoDeInteraccion = 10f;
+    public float rangoAtaque = 2f;
+    public int dañoAtaque = 1;
+    public float fuerzaSalto = 8f;
+
+    public LayerMask capaEnemigos;
+    public LayerMask capaSuelo;
+
+    private Rigidbody2D rb;
+    private bool enSuelo;
+    public Transform sensorSuelo;
+    public float radioSensor = 0.2f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
-       
         float movimiento = Input.GetAxis("Horizontal");
+
+       
         transform.Translate(Vector3.right * movimiento * velocidad * Time.deltaTime);
 
+        
         if (movimiento > 0)
             transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
         else if (movimiento < 0)
             transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
 
-        // Interacción con enemigo más cercano
+        
+        enSuelo = Physics2D.OverlapCircle(sensorSuelo.position, radioSensor, capaSuelo);
+
+        // Saltar
+        if (Input.GetKeyDown(KeyCode.Space) && enSuelo)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, fuerzaSalto);
+        }
+
+        // Interacción
         if (Input.GetKeyDown(KeyCode.R))
         {
             InteractuarConEnemigoMasCercano();
+        }
+
+        // Ataque
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            AtacarEnemigos();
         }
     }
 
@@ -47,6 +79,34 @@ public class Jugador : MonoBehaviour
         else
         {
             Debug.Log("No hay enemigos cercanos.");
+        }
+    }
+
+    private void AtacarEnemigos()
+    {
+        Collider2D[] enemigos = Physics2D.OverlapCircleAll(transform.position, rangoAtaque, capaEnemigos);
+
+        foreach (Collider2D col in enemigos)
+        {
+            ITakeDamage enemigo = col.GetComponent<ITakeDamage>();
+            if (enemigo != null)
+            {
+                enemigo.TomarDaño(dañoAtaque);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Rango de ataque
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rangoAtaque);
+
+        // Sensor de suelo
+        if (sensorSuelo != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(sensorSuelo.position, radioSensor);
         }
     }
 }
